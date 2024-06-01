3c6245cc53dd0a81e8c4edf221d2b019b7f8a603
malinowskid2511 <damian.malinowski2511@gmail.com>
2024-06-01 18:56:22 +0200

    added portfolio and about me pages

---
+++ public/porfiolioScripts.js
@@ -0,0 +1,18 @@
+// var modal = document.getElementById('r1');
+// var modalImg = document.getElementById('rm1');
+// function openModal(img) {
+//     modal.style.display = "block";
+//     modalImg.src = img.src; // Use the same src as the image being clicked
+// }
+//
+// // Function to close the modal
+// function closeModal() {
+//     modal.style.display = "none";
+// }
+//
+// // Close the modal when clicking outside of the image
+// window.onclick = function(event) {
+//     if (event.target === modal) {
+//         modal.style.display = "none";
+//     }
+// }
--- public/scripts.js
+++ public/scripts.js
@@ -25,3 +25,57 @@ function openContactPage() {
 function openPortfolioPage() {
     open("portfolio.html", "_self")
 }
+
+
+const modal1 = document.getElementById('myModal1');
+const modal2 = document.getElementById('myModal2');
+const modal3 = document.getElementById('myModal3');
+const modal4 = document.getElementById('myModal4');
+const modal5 = document.getElementById('myModal5');
+const modalImg1 = document.getElementById('rm1');
+const modalImg2 = document.getElementById('rm2');
+const modalImg3 = document.getElementById('rm3');
+const modalImg4 = document.getElementById('rm4');
+const modalImg5 = document.getElementById('rm5');
+
+function openModal(img) {
+
+
+    if(img.id === "r1") {
+        modal1.style.display = "block";
+        modalImg1.src = "./images/portfolio/rysunki/r1.png";
+
+    } else if(img.id === "r2") {
+        modal2.style.display = "block";
+        modalImg2.src = "./images/portfolio/rysunki/r2.png";
+
+    } else if(img.id === "r3") {
+        modal3.style.display = "block";
+        modalImg3.src = "./images/portfolio/rysunki/r3.png";
+    } else if(img.id === "r4") {
+        modal4.style.display = "block";
+        modalImg4.src = "./images/portfolio/rysunki/r4.png";
+    } else if(img.id === "r5") {
+        modal5.style.display = "block";
+        modalImg5.src = "./images/portfolio/rysunki/r5.png";
+    }
+}
+
+// Function to close the modal
+function closeModal() {
+    modal1.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+    modal4.style.display = "none";
+    modal5.style.display = "none";
+}
+
+// Close the modal when clicking outside of the image
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
